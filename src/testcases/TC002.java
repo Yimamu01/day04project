accdb35269a0a6b68f516b896076b37b116d9328
Yimamu Abudouweili <yimamu01@gmail.com>
2022-07-30 10:21:47 +0200

    002 testcase improved

--- src/testcases/TC002.java
+++ src/testcases/TC002.java
@@ -3,6 +3,7 @@ package testcases;
 public class TC002 {
     public static void main(String[] args) {
         // test case2
-        // write a smoke test
+        // write a smoke test/
+        // sometest is improved
     }
 }
